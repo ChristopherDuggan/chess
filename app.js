676fef2e18f26a82f8b25eed97381dc646dbca00
ChristopherDuggan <christopherduggan@gmail.com>
2025-06-02 12:11:59 -0400

    add start position pieces, render to DOM

--- app.js
+++ app.js
@@ -1,9 +1,29 @@
 const board = new Array(64)
+
+// white pieces
+board[0] = board[7] = 'wR';
+board[1] = board[6] = 'wN';
+board[2] = board[5] = 'wB';
+board[3] = 'wK';
+board[4] = 'wQ';
+for (let i = 8; i < 16; i++) {
+  board[i] = 'wP';
+}
+
+// black pieces
+for (let i = 48; i < 56; i++) {
+  board[i] = 'bP';
+}
+board[56] = board[63] = 'bR';
+board[57] = board[62] = 'bN';
+board[58] = board[61] = 'bB';
+board[59] = 'bK';
+board[60] = 'bQ';
+
 const boardDisplay = document.querySelector('#board-display');
 
 for (let i = 0; i < board.length; i++) {
   const row = Math.floor(i/8);
-  board[i] = i
   const square = document.createElement('div');
   square.classList.add('square');
   if (i % 2 === row % 2) {
@@ -12,6 +32,11 @@ for (let i = 0; i < board.length; i++) {
     square.classList.add('light');
   }
   square.textContent = String.fromCharCode(row + 97) + ((i % 8) + 1);
+  if (board[i]) {
+    const piece = document.createElement('img');
+    piece.src = `images/${board[i]}.svg`;
+    square.appendChild(piece);
+  }
   boardDisplay.appendChild(square)
 }
 
